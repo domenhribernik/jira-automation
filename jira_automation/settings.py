795697794c535a8692dd6394210d4fb8907c01ba
domenhribernik <domen.hribernik4@gmail.com>
2025-04-17 15:59:39 +0300

    Added new domain and SSL

--- jira_automation/settings.py
+++ jira_automation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['45.55.244.142']
+ALLOWED_HOSTS = ['autojiratasks.duckdns.org']
 
 # Application definition
 
