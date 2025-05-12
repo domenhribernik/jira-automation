444d2995b35a045af9e7b7b803f3bc34d7eee553
Domen Hribernik <domen.hribernik4@gmail.com>
2025-05-12 12:59:50 +0000

    fixed concurrency issue with emails

--- jira_automation/settings.py
+++ jira_automation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
-ALLOWED_HOSTS = ['cwjira.duckdns.org', '127.0.0.1', 'localhost', 'web']
+ALLOWED_HOSTS = ['cwjira.duckdns.org', '127.0.0.1', 'localhost', 'web', '159.69.222.52']
 
 # Application definition
 
